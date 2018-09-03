429e896199d84cdd7544ba5183ddf41c37f0a72d
Elias Sarachik <esarachik@bellevue.velocitypartners.net>
2018-09-03 11:07:12 -0300

    add screens

--- GeoPetClient/GeoPetClient/ClientApp/src/App.js
+++ GeoPetClient/GeoPetClient/ClientApp/src/App.js
@@ -4,6 +4,9 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { Mascotas } from './components/Mascotas';
+import { PerdiMascota } from './components/PerdiMascota';
+import { EncontreMascota } from './components/EncontreMascota';
 
 export default class App extends Component {
   displayName = App.name
@@ -13,7 +16,11 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
+		<Route path='/fetchdata' component={FetchData} />
+
+			<Route path='/mascotas' component={Mascotas} />
+			<Route path='/perdi-mascota' component={PerdiMascota} />
+			<Route path='/encotre-mascota' component={EncontreMascota} />
       </Layout>
     );
   }
--- GeoPetClient/GeoPetClient/ClientApp/src/components/NavMenu.js
+++ GeoPetClient/GeoPetClient/ClientApp/src/components/NavMenu.js
@@ -5,37 +5,52 @@ import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
-  displayName = NavMenu.name
+	displayName = NavMenu.name
 
-  render() {
-    return (
-      <Navbar inverse fixedTop fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to={'/'}>GeoPetClient</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to={'/'} exact>
-              <NavItem>
-                <Glyphicon glyph='home' /> Home
+	render() {
+		return (
+			<Navbar inverse fixedTop fluid collapseOnSelect>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<Link to={'/'}>GeoPetClient</Link>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav>
+						<LinkContainer to={'/'} exact>
+							<NavItem>
+								<Glyphicon glyph='home' /> Home
               </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/counter'}>
-              <NavItem>
-                <Glyphicon glyph='education' /> Counter
+						</LinkContainer>
+						<LinkContainer to={'/counter'}>
+							<NavItem>
+								<Glyphicon glyph='education' /> Counter
               </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/fetchdata'}>
-              <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
+						</LinkContainer>
+						<LinkContainer to={'/fetchdata'}>
+							<NavItem>
+								<Glyphicon glyph='th-list' /> Fetch data
               </NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+						</LinkContainer>
+						<LinkContainer to={'/mascotas'}>
+							<NavItem>
+								<Glyphicon glyph='th-list' /> Mascotas
+                </NavItem>
+						</LinkContainer>
+						<LinkContainer to={'/perdi-mascota'}>
+							<NavItem>
+								<Glyphicon glyph='th-list' /> Perdi mi mascota!
+                </NavItem>
+						</LinkContainer>
+						<LinkContainer to={'/encontre-mascota'}>
+							<NavItem>
+								<Glyphicon glyph='th-list' /> Encontre una mascota!
+                </NavItem>
+						</LinkContainer>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+	}
 }
