a6675c33cb7a7f4dd82d2a91d918f9b3a4dbe810
RobertoAssandri <roberto.assandri@gmail.com>
2018-09-03 10:53:03 -0300

    Initial pet controller upload

---
+++ GeoPetClient/GeoPetClient/Controllers/PetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoPetClient.Controllers
+{
+    [Route("api/PetController")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        [HttpGet]
+        public string GetData()
+        {
+            return "hola mundo";
+        }
+    }
+}
